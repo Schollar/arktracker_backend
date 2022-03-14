7e3094e3d6f3a61dc1fd14163bff2f034ca2a07e
Schollar <cschollar22@gmail.com>
2022-03-13 23:56:17 -0600

    Added comments to char tasks sql statements

--- DbInteractions/charTasks.py
+++ DbInteractions/charTasks.py
@@ -4,11 +4,13 @@ import traceback
 
 
 def get_tasks(charId):
+    # Setting up an object to return. Object holds list of tasks.
     user_tasks = {}
     user_tasks['daily'] = []
     user_tasks['weekly'] = []
     conn, cursor = dbh.db_connect()
     try:
+        # Select statement to get task information, as well as character info. Where statement checks to get char DAILY tasks of charId passed to it, where started at is before the next day at 5AM. Task type must be daily and completed at must be null so task is still in progress
         cursor.execute(
             "SELECT task_actions.id, user_tasks.name, user_tasks.description, user_tasks.type, started_at FROM task_actions inner join user_tasks on user_taskId = user_tasks.id WHERE task_actions.characterId = ? and started_at < DATE_ADD(CURDATE() +1 , INTERVAL '05:00' HOUR_MINUTE) and type = 'Daily' and completed_at is NULL", [charId])
         daily_tasks = cursor.fetchall()
@@ -20,6 +22,7 @@ def get_tasks(charId):
                 'taskType': task[3],
                 'started_at': task[4]
             })
+            # Select statement to get task information, as well as character info. Where statement checks to get char WEEKLY tasks of charId passed to it, where started at is before the next thursday at 5AM. Task type must be weekly and completed at must be null so task is still in progress
         cursor.execute(
             "SELECT task_actions.id, user_tasks.name, user_tasks.description, user_tasks.type, started_at FROM task_actions inner join user_tasks on user_taskId = user_tasks.id WHERE task_actions.characterId = ? and started_at < CURDATE() + interval (7 + 3 - weekday(CURDATE())) % 7 day and type = 'Weekly' and completed_at is NULL", [charId])
         weekly_tasks = cursor.fetchall()
@@ -41,5 +44,4 @@ def get_tasks(charId):
         traceback.print_exc()
         print("Something unexpected went wrong")
     dbh.db_disconnect(conn, cursor)
-
     return True, user_tasks
